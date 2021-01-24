d7cdb939e2ea4c7e50f2d440d8b2e20a6eb7478c
antonio <antonioeduardofernandes@gmail.com>
2021-01-24 18:33:47 -0300

    app header created & project size set to vh

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <app-header />
+    <component :is="component" />
+    <bottom-nav />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from "vue"
+import AppHeader from "./components/AppHeader"
+import BottomNav from "./components/BottomNav"
+import Home from "./components/Home"
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    Home,
+    BottomNav,
+    AppHeader,
+  },
+  setup() {
+    const component = ref("Home")
+    return { component }
+  },
 }
 </script>
 
 <style>
+:root {
+  --bg: #1f1f1f;
+  --gold: #d3bc81;
+  --red: #dd5e5e;
+  --orange: #fd953c;
+  --blue: #3a96aa;
+  --green: #05947a;
+  --white: #ffffff;
+  --black: #000000;
+  --glow: 1px 1px 7px rgba(255, 255, 255, 0.26);
+}
+
+body,
+html,
+* {
+  font-family: "Saira Condensed", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  user-select: none;
+  font-size: 2.2vh;
+}
+
+body {
+  height: 100vh;
+  width: 100vw;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
+  color: var(--white);
+  background: var(--bg);
+  overflow: hidden;
+  display: grid;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,173 @@
+<template>
+  <div id="app_header">
+    <div class="info">
+      <div class="endurance">
+        <div class="score">22/22</div>
+        <div class="endurance_icon">
+          <img src="../assets/endurance.png" />
+        </div>
+      </div>
+      <div class="fatigue">
+        <div class="fatigue_icon">
+          <img src="../assets/bolt.png" />
+        </div>
+        <div class="fatigue_score">12</div>
+      </div>
+    </div>
+    <div class="center">
+      <div class="title">Alantariel</div>
+      <div class="subtitle">altos elfos de valfenda</div>
+      <div class="subtitle">andarilho</div>
+      <div class="lock">
+        <img src="../assets/lock.png" />
+      </div>
+    </div>
+    <div class="info">
+      <div class="hope">
+        <div class="hope_score">08/08</div>
+        <div class="hope_icon">
+          <img src="../assets/hope.png" />
+        </div>
+      </div>
+      <div class="shadow">
+        <div class="shadow_icon">
+          <img src="../assets/shadow.png" />
+        </div>
+        <div class="shadow_score">12</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+#app_header {
+  margin-top: 1rem;
+  display: flex;
+  width: 100vw;
+  justify-content: space-around;
+}
+
+.center {
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  text-transform: capitalize;
+  margin-top: 0.6rem;
+}
+
+.lock {
+  --size: 2.6rem;
+  margin-top: 1rem;
+  width: var(--size);
+  height: var(--size);
+  border: 0.18rem solid var(--gold);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0.2;
+}
+
+.lock img {
+  --size: 2rem;
+  width: var(--size);
+  height: var(--size);
+}
+
+.title {
+  font-size: 1.4rem;
+}
+
+.subtitle {
+  opacity: 0.6;
+}
+
+.info {
+  display: flex;
+  flex-flow: column;
+  row-gap: 0.4rem;
+}
+
+.endurance,
+.hope {
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  row-gap: 0.4rem;
+}
+
+.endurance_icon,
+.hope_icon {
+  --size: 3.4rem;
+  width: var(--size);
+  height: var(--size);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 0.24rem solid var(--green);
+}
+
+.fatigue,
+.shadow {
+  align-self: flex-end;
+  position: relative;
+}
+
+.fatigue_icon,
+.shadow_icon {
+  --size: 2.4rem;
+  width: var(--size);
+  height: var(--size);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 0.16rem solid var(--orange);
+}
+
+.fatigue_score,
+.shadow_score {
+  --size: 1.8rem;
+  width: var(--size);
+  height: var(--size);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: var(--bg);
+  border: 0.1rem solid var(--white);
+  font-size: 0.9rem;
+  position: absolute;
+  right: -10px;
+  bottom: -16px;
+}
+
+.hope {
+  position: relative;
+}
+
+.hope_icon {
+  border-color: var(--white);
+}
+
+.shadow {
+  align-self: flex-start;
+}
+
+.shadow_icon {
+  border-color: var(--red);
+}
+
+.shadow_score {
+  left: -10px;
+}
+</style>
